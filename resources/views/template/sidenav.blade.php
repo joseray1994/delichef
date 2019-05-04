48f1c2096594229c5869032e24e0d1a48e8877a1
Ray Gamboa <raymundoIT@yascemail.com>
2019-05-04 13:03:01 -0500

    Integrate template with laravel 5.6
    create base for style of the project "Deli Chef"

---
+++ resources/views/template/sidenav.blade.php
@@ -0,0 +1,24 @@
+<div class="navbar-content">
+            <!-- start: SIDEBAR -->
+            <div class="main-navigation navbar-collapse collapse">
+                <!-- start: MAIN MENU TOGGLER BUTTON -->
+                <div class="navigation-toggler">
+                    <i class="clip-chevron-left"></i>
+                    <i class="clip-chevron-right"></i>
+                </div>
+                <!-- end: MAIN MENU TOGGLER BUTTON -->
+                <!-- start: MAIN NAVIGATION MENU -->
+                <ul class="main-navigation-menu">
+                    <li>
+                        <!--active open-->
+                        <a href="index.html">
+                            <i class="clip-home-3"></i>
+                            <span class="title"> Dashboard </span><span class="selected"></span>
+                        </a>
+                    </li>
+                   
+                </ul>
+                <!-- end: MAIN NAVIGATION MENU -->
+            </div>
+            <!-- end: SIDEBAR -->
+        </div>
